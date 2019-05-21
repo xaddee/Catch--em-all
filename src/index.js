74dc97a0c1411fc43a6a0979c9c303e9fe4f6117
Adelin Nedelcu <n.adelin0223@gmail.com>
2019-05-21 23:59:20 +0300

    Remove defaults

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
 
 const config = {
   type: Phaser.AUTO,
@@ -8,25 +7,21 @@ const config = {
   height: 600,
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  
+}
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+function update() {
+
 }
